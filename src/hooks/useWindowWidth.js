fba0c12ef26e49989a4b69dddfd7fefb6fb991f3
briandridge <briandridge@gmail.com>
2019-11-14 15:20:25 -0700

    maybe fix window problem?

--- src/hooks/useWindowWidth.js
+++ src/hooks/useWindowWidth.js
@@ -4,6 +4,10 @@ export default function useWindowWidth() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
   useEffect(() => {
+    if (typeof window !== "undefined") {
+      console.log(`Location: ${window.location.href}`)
+    }
+
     const listener = () => setWindowWidth(window.innerWidth)
 
     window.addEventListener("resize", listener)
